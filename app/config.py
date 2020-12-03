eb828a749d83c43a3cb21d81b36df7a7f5b9fb4b
YOGESHWARAN R <yogeshin247@gmail.com>
2020-12-03 18:58:29 +0530

    secret key added to env

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Configaration:
-    SECRET_KEY = "mywebsiteuseflaskapp"
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASK_ADMIN_SWATCH = "cerulean"
